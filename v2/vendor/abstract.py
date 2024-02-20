bfd0bb745f23948e7db4355f79545a147855ecb8
Ryan Hamel <ryan@rkhtech.org>
2024-02-19 18:30:59 -0800

    Implement abstractions correctly

--- v2/vendor/abstract.py
+++ v2/vendor/abstract.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABCMeta, abstractmethod
 
 from models.data.arp import ARP_Table
 from models.data.facts import Device_Facts
@@ -13,37 +13,79 @@ various scripts when it needs to know if its capable of X feature set by
 way of 'if isinstance(variable, L3)' branching.
 '''
 
-class Config_MGMT(ABC):
+class Config_MGMT(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'commit_config') and 
+                callable(subclass.commit_config) or 
+                NotImplemented)
+
     @abstractmethod
     async def commit_config(self, parameters: list = []) -> bool:
-        pass
+        """Commit configuration changes to the device"""
+        raise NotImplementedError
 
-class Firmware(ABC):
+class Firmware(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_facts') and 
+                callable(subclass.get_facts) or 
+                NotImplemented)
+
     @abstractmethod
     async def get_facts(self) -> Device_Facts:
-        pass
+        """Get very basic information about a device"""
+        raise NotImplementedError
 
-class Hardware(ABC):
-    pass
+class Hardware(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (NotImplemented)
 
-class L2(ABC):
+class L2(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_lldp_neighbors') and 
+                callable(subclass.get_lldp_neighbors) or 
+                NotImplemented)
+
     @abstractmethod
     async def get_lldp_neighbors(self, type) -> LLDP_Neighbor_Table:
-        pass
+        """Return a list of LLDP neighbors"""
+        raise NotImplementedError
 
-class L3(ABC):
+class L3(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_arp_table') and 
+                callable(subclass.get_arp_table) and
+                hasattr(subclass, 'get_nd_table') and 
+                callable(subclass.get_nd_table) or 
+                NotImplemented)
+
     @abstractmethod
     async def get_arp_table(self) -> ARP_Table:
-        pass
-    
+        """Get the devices IPv4 ARP table"""
+        raise NotImplementedError
+
     @abstractmethod
     async def get_nd_table(self) -> ND_Table:
-        pass
+        """Get the IPv6 neighbors table"""
+        raise NotImplementedError
 
-class Routing(ABC):
-    pass
+class Routing(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (NotImplemented)
 
-class Security(ABC):
+class Security(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_banner') and 
+                callable(subclass.get_banner) or 
+                NotImplemented)
+
     @abstractmethod
     async def get_banner(self, type) -> Device_Banner:
-        pass
+        """Retrieve the devices login or motd banner"""
+        raise NotImplementedError
